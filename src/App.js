445550ff2f686444732be199dc8754a0819c6aca
Fida <722003hussainfida@gmail.com>
2023-10-28 20:32:17 +0530

    Responsive_Login_page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
+import { toast } from "react-toastify";
 
 function App() {
   const intialState = {
     email: "",
     password: "",
-    checkbox: false,
+    remember: false,
   };
   const [view, setView] = useState(false);
   const [logInput, setLogInput] = useState(intialState);
@@ -17,7 +18,7 @@ function App() {
   const handleChange = (e) => {
     console.log(e);
     setLogInput({ ...logInput, [e.target.name]: e.target.value });
-    if (e.target.name === "checkbox") {
+    if (e.target.name === "remember") {
       setLogInput({ ...logInput, [e.target.name]: e.target.checked });
     }
   };
@@ -27,11 +28,12 @@ function App() {
 
     // Send this JSON data into backend
     console.log(logInput);
+    toast.success("Login Successfully");
 
     setLogInput(intialState);
   };
   return (
-    <div className="w-screen h-screen flex justify-center items-center flex-col md:flex-row    ">
+    <div className="w-screen h-screen flex justify-center items-center flex-col md:flex-row  text-white   ">
       <div className="w-2/3 h-1/3 flex items-center justify-center mb-6 object-cover md:h-2/4 md:w-full">
         <img
           src="man.png"
@@ -39,14 +41,11 @@ function App() {
           className="object-center h-full w-fit "
         />
       </div>
-      <div className=" bg-red-200 w-full h-full py-12 flex flex-col md:w-2/4 md:justify-center md:items-center ">
+      <div className=" bg-[rgb(108,99,255)] w-full h-full py-12 flex flex-col md:w-2/4 md:justify-center md:items-center ">
         <form className="w-3/4 mx-auto" onSubmit={handleSubmit}>
-          <h1 className="text-2xl  w-fit mx-auto font-bold">Login</h1>
+          <h1 className="text-2xl  w-fit mx-auto font-bold  mb-6">Login</h1>
           <div className="mb-6">
-            <label
-              htmlFor="email"
-              className="block mb-2 text-sm  text-gray-900 font-bold"
-            >
+            <label htmlFor="email" className="block mb-2 text-sm   font-bold">
               Your email
             </label>
             <input
@@ -54,7 +53,7 @@ function App() {
               id="email"
               name="email"
               value={logInput.email}
-              className="   text-sm rounded-lg  p-2.5 w-full border border-gray-400 focus:ring-blue-900 focus:border-blue-500"
+              className="   text-sm rounded-lg  p-2.5 w-full border text-gray-900 border-gray-400 focus:ring-blue-900 focus:border-blue-500"
               placeholder="name@gmail.com"
               onChange={handleChange}
               required
@@ -63,7 +62,7 @@ function App() {
           <div className="mb-6 relative">
             <label
               htmlFor="password"
-              className="block mb-2 text-sm  text-gray-900 font-bold  "
+              className="block mb-2 text-sm  font-bold  "
             >
               Your password
             </label>
@@ -73,12 +72,12 @@ function App() {
               name="password"
               value={logInput.password}
               placeholder="password"
-              className="   text-sm rounded-lg  p-2.5 w-full border border-gray-400  placeholder:text-gray-600"
+              className="   text-sm rounded-lg text-gray-900 p-2.5 w-full border border-gray-400  "
               required
               onChange={handleChange}
             />
             <span
-              className="absolute right-3 top-10 cursor-pointer"
+              className="absolute right-3 top-10 cursor-pointer text-[rgb(108,99,255)]"
               onClick={handleView}
             >
               {view ? <AiFillEye /> : <AiFillEyeInvisible />}
@@ -87,32 +86,37 @@ function App() {
           <div className="flex items-start justify-between mb-6">
             <div className="flex items-center  h-5">
               <input
-                id="checkbox"
-                name="checkbox"
+                id="remember"
+                name="remember"
                 type="checkbox"
-                checked={logInput.checkbox}
+                checked={logInput.remember}
                 className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                onClick={handleChange}
+                onChange={handleChange}
               />
               <label
                 htmlFor="remember"
-                className="block ml-2 text-sm  text-gray-900 font-bold  "
+                className="block ml-2 text-sm   font-bold  "
               >
                 Remember me
               </label>
             </div>
 
-            <span className="text-orange-500 text-sm ">forgot password ?</span>
+            <span className="text-orange-500 cursor-pointer text-sm hover:underline font-semibold">
+              forgot password ?
+            </span>
           </div>
           <button
             type="submit"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full  px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="text-[rgb(108,99,255)] font-bold bg-white  focus:ring-4 focus:outline-none focus:ring-blue-300  rounded-lg text-sm w-full border border-white px-5 py-2.5 text-center hover:bg-[rgb(108,99,255)] hover:text-white"
           >
             Login
           </button>
         </form>
         <h2 className="text-center mt-6">
-          Don't have account ? <span className="text-orange-500">Register</span>
+          Don't have account ?{" "}
+          <span className=" cursor-pointer  text-orange-500 font-semibold hover:underline">
+            Register
+          </span>
         </h2>
       </div>
     </div>
